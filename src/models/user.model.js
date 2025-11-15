9bcdaa681a305121bbb5f588ca383a01ce9767cf
Ibrahim Traore <iboutraore15@gmail.com>
2025-11-14 23:05:07 -0500

    ajout des modeles Sequelize user-company-application

---
+++ src/models/user.model.js
@@ -0,0 +1,13 @@
+import { DataTypes } from "sequelize";
+import { dataBase } from "../database.js";
+
+const User = dataBase.define("user", {
+  user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  first_name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  status: DataTypes.STRING,
+  id_company: DataTypes.INTEGER
+});
+
+export default User;
